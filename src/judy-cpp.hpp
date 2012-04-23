e294d55ce2667fe65d31a1a9c42b96c70f64d702
Johannes Huning <mail@johanneshuning.com>
2012-04-24 00:12:59 +0200

    Add first typedefs, stub method `insert`

--- src/judy-cpp.hpp
+++ src/judy-cpp.hpp
@@ -1,12 +1,32 @@
 #ifndef JUDY_CPP_HPP
 #define JUDY_CPP_HPP JUDY_CPP_HPP
 
+#include <memory>
+#include <iostream>
+
+
 namespace judy
 {
 
-template <class E, class A = std::allocator<E> >
+template <typename T,
+    typename A = std::allocator<T> >
 class hs
 {
+public:
+    typedef T           value_type;
+    typedef value_type  key_type;
+    typedef A           allocator;
+    typedef value_type* pointer;
+    typedef value_type& reference;
+    typedef std::size_t size_type;
+
+
+public:
+    inline bool
+    insert(reference key, reference value)
+    {
+        return false;
+    }
 };
 
 }
